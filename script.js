9de7f8376087e8ed421e14b952b3a7c3940f2875
Filipe Freire <filipefreire@protonmail.com>
2020-07-14 17:18:56 +0100

    Fix pause mechanism on click of the button

--- script.js
+++ script.js
@@ -5,21 +5,32 @@ window.addEventListener('load', () => {
   const startButton = document.getElementById('startGame');
   let gameHasStarted = false;
   const startGameSound = () => game.clickStartSound.play();
-
+  /*
   startButton.onclick = function startGane() {
     if (gameHasStarted === false) {
       startGameSound();
       game.playMusic();
       gameHasStarted = true;
       startButton.innerHTML = 'Pause';
-      game.isPaused = false;
       game.loop();
-    } else {
-      game.gameMusic.pause();
-      game.isPaused = true;
-      startButton.innerHTML = 'Start';
     }
   };
+*/
+  startButton.addEventListener('click', function () {
+    if (gameHasStarted === false) {
+      startGameSound();
+      game.playMusic();
+      gameHasStarted = true;
+      startButton.innerHTML = 'Pause';
+      game.loop();
+    } else if (!game.isPaused) {
+      startButton.innerHTML = 'Start';
+      game.togglePause();
+    } else if (game.isPaused) {
+      startButton.innerHTML = 'Pause';
+      game.togglePause();
+    }
+  });
 
   /* if (gameHasStarted === false && startButton.onclick()) {
     gameHasStarted = true;
